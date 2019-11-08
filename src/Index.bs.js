c299980d7b70480da6bf6d1ff20790611fad901a
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-08 18:53:16 +0800

    Create Index.bs.js

---
+++ src/Index.bs.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var React = require("react");
+var ReactDom = require("react-dom");
+
+ReactDom.render(React.createElement("hr", undefined), document.body);
+
+/*  Not a pure module */
